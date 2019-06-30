2992189c3daff624ffd1d57ce9b0a792d2fcb80c
Lorenzo <hyperl@hotmail.it>
2019-07-01 00:31:17 +0200

    fixed assignment 7

--- assignments/Carnaghi/A07- Around the world/World.js
+++ assignments/Carnaghi/A07- Around the world/World.js
@@ -12,9 +12,9 @@ function updateWorld(rvx, rvy, rvz) {
 	rvz = rvz*degToRad;
 	
 	//calculate the 3 quaternions used for x, y and z rotations
-	var quatX = new Quaternion(Math.cos(rvx), Math.sin(rvx), 0, 0);
-	var quatY = new Quaternion(Math.cos(rvy), 0, Math.sin(rvy), 0);
-	var quatZ = new Quaternion(Math.cos(rvz), 0, 0, Math.sin(rvz));
+	var quatX = new Quaternion(Math.cos(rvx/2), Math.sin(rvx/2), 0, 0);
+	var quatY = new Quaternion(Math.cos(rvy/2), 0, Math.sin(rvy/2), 0);
+	var quatZ = new Quaternion(Math.cos(rvz/2), 0, 0, Math.sin(rvz/2));
 	
 	// p' = qx*qy*qz*p
 	currP = quatX.mul(quatY.mul(quatZ.mul(currP)));
